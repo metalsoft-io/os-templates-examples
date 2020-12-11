e3bb5d8872bea3baa140c507f6de398cd5661f68
aurelian.anghel <aurelian.anghel@bigstep.com>
2020-12-11 17:07:36 +0200

    update centos8 template

--- CentOS/create_centos_8_template.sh
+++ CentOS/create_centos_8_template.sh
@@ -14,22 +14,33 @@ TEMPLATE_VERSION="$2"
 TEMPLATE_DISPLAY_NAME="CentOS $TEMPLATE_VERSION"
 TEMPLATE_DESCRIPTION="$TEMPLATE_DISPLAY_NAME"
 TEMPLATE_LABEL=$1
-TEMPLATE_ROOT="/centos/$TEMPLATE_VERSION"
+TEMPLATE_ROOT="centos/$TEMPLATE_VERSION"
 
 
 SOURCES="./$TEMPLATE_VERSION"
 
 MC="metalcloud-cli"
 
-DATACENTER_NAME="uk-reading2"
-REPO_URL=`metalcloud-cli datacenter get --id $DATACENTER_NAME -show-config -format json | jq ".[0].CONFIG | fromjson |.repoURLRoot" -r`
+DATACENTER_NAME="$METALCLOUD_DATACENTER"
+REPO_URL=`metalcloud-cli datacenter get --id $DATACENTER_NAME --show-config --format json | jq ".[0].CONFIG | fromjson |.repoURLRoot" -r`
 TEMPLATE_BASE=$REPO_URL/$TEMPLATE_ROOT
 
-$MC os-template delete -id "$TEMPLATE_LABEL" --autoconfirm  2>&1 >/dev/null || true
+if $MC os-template get --id "$TEMPLATE_LABEL" 2>&1 >/dev/null; then
+    if $MC os-template delete --id "$TEMPLATE_LABEL" --autoconfirm 2>&1 >/dev/null; then
+        OS_TEMPLATE_COMMAND=create
+        OS_TEMPLATE_FLAG=label
+    else
+        OS_TEMPLATE_COMMAND=update
+        OS_TEMPLATE_FLAG=id
+    fi
+else
+    OS_TEMPLATE_COMMAND=create
+    OS_TEMPLATE_FLAG=label
+fi
 
 #create the template
-$MC os-template create \
---label "$TEMPLATE_LABEL" \
+$MC os-template $OS_TEMPLATE_COMMAND \
+--$OS_TEMPLATE_FLAG "$TEMPLATE_LABEL" \
 --display-name "$TEMPLATE_DISPLAY_NAME" \
 --description "$TEMPLATE_DESCRIPTION" \
 --boot-type uefi_only \
@@ -46,31 +57,31 @@ $MC os-template create \
 #third param is usage
 function addBinaryURLAsset {
         
-    $MC asset list -format json  | jq ".[] | select(.FILENAME==\"${1}-${TEMPLATE_LABEL}\")|.ID" -r | xargs metalcloud-cli asset delete --autoconfirm -id
-    $MC asset create -url "$TEMPLATE_BASE/$2" -filename "$1-$TEMPLATE_LABEL" -mime "application/octet-stream" -usage "$3"
-    $MC asset associate -id "$1-$TEMPLATE_LABEL" -template-id $TEMPLATE_LABEL -path "/$1"
+    $MC asset list --format json  | jq ".[] | select(.FILENAME==\"${1}-${TEMPLATE_LABEL}\")|.ID" -r | xargs -l metalcloud-cli asset delete --autoconfirm --id
+    $MC asset create --url "$TEMPLATE_BASE/$2" --filename "$1-$TEMPLATE_LABEL" --mime "application/octet-stream" --usage "$3" --return-id
+    $MC asset associate --id "$1-$TEMPLATE_LABEL" --template-id $TEMPLATE_LABEL -path "/$1"
 }
 
 #firt param is file name on disk
 #second param is path in tftp/http
 #third param is params accepted
 function addFileAsset {
-    $MC asset list -format json  |jq ".[] | select(.FILENAME==\"${1}-$TEMPLATE_LABEL\")|.ID" -r | xargs metalcloud-cli asset delete --autoconfirm -id
-    cat $SOURCES/$1 | $MC asset create -filename "$1-$TEMPLATE_LABEL" -mime "text/plain" -variable-names-required "$3" -pipe
-    $MC asset associate -id "$1-$TEMPLATE_LABEL" -template-id $TEMPLATE_LABEL -path "$2"
+    $MC asset list --format json  |jq ".[] | select(.FILENAME==\"${1}-$TEMPLATE_LABEL\")|.ID" -r | xargs -l metalcloud-cli asset delete --autoconfirm --id
+    cat $SOURCES/$1 | $MC asset create --filename "$1-$TEMPLATE_LABEL" --mime "text/plain" --pipe
+    $MC asset associate --id "$1-$TEMPLATE_LABEL" --template-id $TEMPLATE_LABEL --path "$2"
 }
 
 #add bootx64 (pre-bootloader uefi for secure boot)
-addBinaryURLAsset "bootx64.efi" "BaseOS/x86_64/kickstart/EFI/BOOT/BOOTX64.EFI" "bootloader"
+TEMPLATE_INSTALL_BOOTLOADER_ASSET=`addBinaryURLAsset "bootx64.efi" "BaseOS/x86_64/kickstart/EFI/BOOT/BOOTX64.EFI" "bootloader"`
 
 #set the bootx64 bootloader as the template's default bootloader
-metalcloud-cli os-template update -id "$TEMPLATE_LABEL" -install-bootloader-asset "bootx64.efi-$TEMPLATE_LABEL"
+metalcloud-cli os-template update --id "$TEMPLATE_LABEL" --install-bootloader-asset "$TEMPLATE_INSTALL_BOOTLOADER_ASSET"
 
 #add grub bootloader
 addBinaryURLAsset "grubx64.efi" "BaseOS/x86_64/kickstart/EFI/BOOT/grubx64.efi"
 
 #add grub config file
-addFileAsset 'grubx64.cfg' '/grub.cfg' "wan_interface_mac_adddress_0,repo_url_root,HTTP_SERVER_ENDPOINT"
+addFileAsset 'grubx64.cfg' '/grub.cfg'
 
 #add vmlinuz
 addBinaryURLAsset 'vmlinuz' 'BaseOS/x86_64/kickstart/images/pxeboot/vmlinuz'
@@ -79,8 +90,8 @@ addBinaryURLAsset 'vmlinuz' 'BaseOS/x86_64/kickstart/images/pxeboot/vmlinuz'
 addBinaryURLAsset 'initrd.img' 'BaseOS/x86_64/kickstart/images/pxeboot/initrd.img'
 
 #add kickstart file ks.cfg
-addFileAsset 'ks.cfg' '/ks.cfg' "repo_url_root,wan_interface_mac_adddress_0,instance_subdomain_permanent,initial_password,ntp_server_ip_0,HTTP_SERVER_ENDPOINT,management_ssh_key" 
+addFileAsset 'ks.cfg' '/ks.cfg'
 
 #add snmpd.conf
-addFileAsset 'snmpd.conf' '/snmpd.conf' "snmp_port,snmp_security_name,snmp_authentication_protocol,snmp_authentication_passphrase,snmp_privacy_protocol,snmp_privacy_passphrase,snmp_community_password,snmp_security_name,datacenter_name"
+addFileAsset 'snmpd.conf' '/snmpd.conf'
 
